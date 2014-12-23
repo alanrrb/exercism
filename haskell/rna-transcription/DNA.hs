37732f9115ecb27e0e1446a1a93832ff88ad9f0c
Alan R. R. Batista <alanrrb@gmail.com>
2014-12-23 21:53:38 -0200

    haskell rna transcription

---
+++ haskell/rna-transcription/DNA.hs
@@ -0,0 +1,9 @@
+module DNA (toRNA) where
+
+toRNA :: String -> String
+toRNA = map nucleotideToRNA
+  where nucleotideToRNA 'C' = 'G'
+        nucleotideToRNA 'G' = 'C'
+        nucleotideToRNA 'A' = 'U'
+        nucleotideToRNA 'T' = 'A'
+
